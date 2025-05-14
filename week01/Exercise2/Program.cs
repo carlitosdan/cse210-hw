b588a4be7d0a1612b94a917c57fc18e73f0276f1
carlitosdan <cardafma@gmail.com>
2025-05-13 21:09:29 -0500

    v1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter  you grade percentage: ");
+        string gp = Console.ReadLine();
+        char sd = gp[1];
+        int gradePercent = int.Parse(gp);
+        int secondDigit = int.Parse(sd.ToString());
+        string letter = "";
+
+        if(gradePercent >= 90){
+            letter = "A";
+        }
+        else if(gradePercent >= 80){
+            letter = "B";
+        }
+        else if(gradePercent >= 70){
+            letter = "C";
+        }
+        else if(gradePercent >= 60){
+            letter = "D";
+        }
+        
+        if (secondDigit >= 7){
+                letter += "+";
+            }
+            if (secondDigit < 3){
+                letter += "-";
+            }
+
+        if(gradePercent < 60){
+            letter = "F";
+        } 
+           
+        Console.WriteLine($"Your grade is {letter}");
+
+        if(gradePercent >= 70){
+            Console.WriteLine("Congratulations! You passed the course");
+
+        }
+
+
+
+
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("What is the magic number? ");
+        string mn = Console.ReadLine();
+
+        Console.WriteLine("What is your guess?");
+        string yn = Console.ReadLine();
+
+        int magicNumber = int.Parse(mn);
+        int yourNumber = int.Parse(yn);
+
+        if(yourNumber == magicNumber){
+            Console.WriteLine("You guessed it!");
+        }
+        else if(yourNumber > magicNumber){
+            Console.WriteLine("Higher");
+        }
+        else if(yourNumber < magicNumber){
+            Console.WriteLine("Lower");
+        }
+        
+
+
+
+
     }
 }
