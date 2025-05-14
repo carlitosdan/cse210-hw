5241b11fcb385a2f096cf54d673bc1483852b323
carlitosdan <cardafma@gmail.com>
2025-05-14 10:10:14 -0500

    first loop

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,23 +6,37 @@ class Program
     {
         Console.WriteLine("What is the magic number? ");
         string mn = Console.ReadLine();
-
-        Console.WriteLine("What is your guess?");
-        string yn = Console.ReadLine();
-
         int magicNumber = int.Parse(mn);
-        int yourNumber = int.Parse(yn);
 
+        int yourNumber = 0;
+
+        
+
+       while(magicNumber != yourNumber ){
+            Console.WriteLine("What is your guess?");
+            string yn = Console.ReadLine();
+            yourNumber = int.Parse(yn);
+
+            if(yourNumber > magicNumber){
+                Console.WriteLine("Higher");
+            }
+            else if(yourNumber < magicNumber){
+                Console.WriteLine("Lower");
+            }
+
+
+       }
+       
         if(yourNumber == magicNumber){
             Console.WriteLine("You guessed it!");
         }
-        else if(yourNumber > magicNumber){
-            Console.WriteLine("Higher");
-        }
-        else if(yourNumber < magicNumber){
-            Console.WriteLine("Lower");
-        }
         
+
+
+
+       
+        
+
 
 
 
