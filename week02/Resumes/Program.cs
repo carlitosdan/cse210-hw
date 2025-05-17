e0a0a01d07a9daf63f1d71188a6ceb6d6ee57881
carlitosdan <cardafma@gmail.com>
2025-05-17 08:22:07 -0500

    v1

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+
+        job1._company = "Mirosoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 1990;
+        job1._endYear = 2012;
+
+
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        job2.DisplayJobDetails();
+
+        Resume myresume = new Resume();
+        myresume._jobs.Add(job1);
+        
+
+        string x = myresume._jobs[0]._jobTitle;
+
+        Console.WriteLine(x);
+        
+
+
+
+
+
+
+
+        
+
+
+
     }
 }
---
+++ week02/Resumes/resume.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+       public class Resume(){
+           
+           public string _name = "";
+           public List<Job> _jobs = new List<Job>();
+
+
+           public void displayResume()
+            {
+            // Console.WriteLine($"{_jobTitle} ({_company})  {_startYear} - {_endYear}");
+            }
+        
+
+       }
+       
+    
